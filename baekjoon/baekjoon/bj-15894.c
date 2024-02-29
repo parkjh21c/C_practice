8e7489585c47732a289c14bd429a0901220a4d24
Park Ji-Ho <jiho010920@gmail.com>
2024-02-29 18:31:59 +0900

    bj

---
+++ baekjoon/baekjoon/bj-15894.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main() {
+	int n;
+
+	scanf("%d", &n);
+
+	printf("%ld", n * 4);
+
+
+	return 0;
+}
---
+++ baekjoon/baekjoon/bj-27323.c
@@ -0,0 +1,13 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+
+int main() {
+	int A, B;
+
+	scanf("%d", &A);
+	scanf("%d", &B);
+
+	printf("%d", A * B);
+
+	return 0;
+}
