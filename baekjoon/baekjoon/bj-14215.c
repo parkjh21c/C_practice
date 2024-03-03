2bb18040a6e4220603568b18e708350bca24973d
Park Ji-Ho <jiho010920@gmail.com>
2024-03-03 20:33:24 +0900

    bj-14215

---
+++ baekjoon/baekjoon/bj-14215.c
@@ -0,0 +1,25 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+// 14215 ¹ø
+int main() {
+	int num[3], max_index;
+
+	scanf("%d %d %d", &num[0], &num[1], &num[2]);
+
+	if (num[0] >= num[1] + num[2]) {
+		printf("%d", 2 * (num[1] + num[2]) - 1);
+	}
+	else if (num[1] >= num[0] + num[2]) {
+		printf("%d", 2 * (num[0] + num[2]) - 1);
+	}
+	else if (num[2] >= num[0] + num[1]) {
+		printf("%d", 2 * (num[0] + num[1]) - 1);
+	}
+	else {
+		printf("%d", num[0] + num[1] + num[2]);
+	}
+
+
+	return 0;
+}
