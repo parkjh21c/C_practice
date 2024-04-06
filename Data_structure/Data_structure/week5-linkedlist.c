cdaa4ec9200bcfc43a77a766d50dd546aa874431
Park Ji-Ho <jiho010920@gmail.com>
2024-04-06 16:29:55 +0900

    linkedlist

--- Data_structure/Data_structure/week5-linkedlist.c
+++ Data_structure/Data_structure/week5-linkedlist.c
@@ -32,6 +32,87 @@ void insertFirst(ListType* L, element e) {
 	L->size++;
 }
 
+void insertLast(ListType* L, element e) {
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));	// 새롭게 만들어지는 노드
+	node->data = e;
+	node->next = NULL;
+
+	if (isEmpty(L))	// if(L->size == 0)
+		L->head = node;
+	else {
+		ListNode* p;
+		for (p = L->head; p->next != NULL; p = p->next);
+
+		p->next = node;
+	}
+	L->size++;
+}
+
+void insert(ListType* L, int pos, element e) {
+	if (pos == 1)
+		insertFirst(L, e);
+	else if (pos == L->size + 1)
+		insertLast(L, e);
+	else {
+		ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+		ListNode* p = L->head;
+
+		for (int i = 1; i < pos - 1; i++)
+			p = p->next;
+
+		node->data = e;
+		node->next = p->next;
+		p->next = node;
+
+		L->size++;
+	}
+}
+
+element deleteFirst(ListType* L) {
+	if (isEmpty(L)) {
+		printf("No Element To Delete.\n");
+		return -1;
+	}
+	ListNode* p = L->head;
+	element e = p->data;
+	L->head = p->next;
+	free(p);
+	L->size--;
+
+	return e;
+}
+
+element delete(ListType* L, int pos) {
+	if (isEmpty(L)) {
+		printf("No Element To Delete.\n");
+		return -1;
+	}
+
+	if (pos < 1 || pos > L->size) {
+		printf("Invalid Position.\n");
+		return -1;
+	}
+	else {
+		ListNode* p = L->head;
+		ListNode* q = NULL;
+		element e;
+
+		if (pos == 1)
+			e = deleteFirst(L);
+		else {
+			for (int i = 1; i < pos; i++) {
+				q = p;
+				p = p->next;
+			}
+			e = p->data;
+			q->next = p->next;
+			free(p);
+			L->size--;
+		}
+		return e;
+	}
+}
+
 void print(ListType* L) {
 	ListNode* p;
 	for (p = L->head; p != NULL; p = p->next)
@@ -44,8 +125,19 @@ int main() {
 	ListType L;
 	init(&L);
 
+	insertLast(&L, 'C'); print(&L);
+
 	insertFirst(&L, 'A'); print(&L);
 	insertFirst(&L, 'B'); print(&L);
+
+	insertLast(&L, 'E'); print(&L); getchar();
+
+	insert(&L, 1, 'D'); print(&L);
+	insert(&L, 3, 'F'); print(&L);
+	insert(&L, 7, 'G'); print(&L); getchar();
+
+	printf("[%c] id deleted.\n", deleteFirst(&L)); print(&L);
+	printf("[%c] id deleted.\n", delete(&L, 1)); print(&L);
 
 	return 0;
 }
