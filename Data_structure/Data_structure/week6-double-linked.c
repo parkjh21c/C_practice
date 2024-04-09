5134c1faf04255a5e397068daa11613320552c1c
Park Ji-Ho <jiho010920@gmail.com>
2024-04-09 17:44:45 +0900

    headnode

--- Data_structure/Data_structure/week6-double-linked.c
+++ Data_structure/Data_structure/week6-double-linked.c
@@ -85,6 +85,81 @@ void insert(DListType* DL, int pos, element e) {
 }
 
 // delete함수는 만들어 볼 것
+// 삭제 후 노드가 없다면 => init 함수 호출 필요
+element deleteFirst(DListType* DL) {
+	if (isEmpty(DL)) {
+		printf("No Element To Delete.\n");
+		return -1;
+	}
+	
+	DListNode* p = DL->H;
+	element e = p->data;
+	DL->H = p->next;
+	p->next->prev = NULL;
+	free(p);
+
+	DL->count--;
+
+	if (isEmpty(DL))
+		init(DL);
+
+	return e;
+}
+
+element deleteLast(DListType* DL) {
+	if (isEmpty(DL)) {
+		printf("No Element To Delete.\n");
+		return -1;
+	}
+
+	DListNode* p = DL->T;
+	element e = p->data;
+	DL->T = p->prev;
+	p->prev->next = NULL;
+	free(p);
+
+	DL->count--;
+
+	if (isEmpty(DL))
+		init(DL);
+
+	return e;
+}
+
+element delete(DListType* DL, int pos) {
+	if (isEmpty(DL)) {
+		printf("No Element To Delete.\n");
+		return -1;
+	}
+	else if (pos < 1 || pos > DL->count) {
+		printf("Invalid Position.\n");
+		return -1;
+	}
+	else {
+		DListNode* p = DL->H;
+		element e;
+
+		if (pos == 1)
+			e = deleteFirst(DL);
+		else if (pos == DL->count)
+			e = deleteLast(DL);
+		else {
+			for (int i = 1; i < pos; i++)
+				p = p->next;
+
+			e = p->data;
+			p->next->prev = p->prev;
+			p->prev->next = p->next;
+			free(p);
+
+			DL->count--;
+		}
+		if (isEmpty(DL))
+			init(DL);
+
+		return e;
+	}
+}
 
 void print(DListType* DL) {
 	for (DListNode* p = DL->H; p != NULL; p = p->next)
@@ -106,7 +181,16 @@ int main() {
 
 	insert(&DL, 1, 70); print(&DL);
 	insert(&DL, DL.count + 1, 100); print(&DL);
-	insert(&DL, 3, 110); print(&DL);
+	insert(&DL, 3, 110); print(&DL); getchar();
+
+	deleteFirst(&DL); print(&DL);
+	deleteFirst(&DL); print(&DL);
+
+	deleteLast(&DL); print(&DL);
+	deleteLast(&DL); print(&DL); getchar();
+
+	delete(&DL, 3); print(&DL);
+	delete(&DL, 2); print(&DL);
 
 	return 0;
 }
---
+++ Data_structure/Data_structure/week6-headnode.c
@@ -0,0 +1,78 @@
+#pragma warning(disable: 4996)
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct DListNode {
+	element data;
+	struct DListNode* prev;
+	struct DListNode* next;
+}DListNode;
+
+void init(DListNode* H, DListNode* T) {
+	H->next = T;
+	T->prev = H;
+}
+
+// 헤드 포인터 함수는 insertFirst함수가 없으면 p를 포인터에 둘 수 없다
+
+void insert(DListNode* H, int pos, element e) {
+	DListNode* p = H;
+
+	for (int i = 1; i < pos; i++)
+		p = p->next;
+
+	DListNode* node = (DListNode*)malloc(sizeof(DListNode));
+
+	node->data = e;
+	node->prev = p;
+	node->next = p->next;
+
+	p->next->prev = node;
+	p->next = node;
+}
+
+// delete 함수 구현해볼것
+element delete(DListNode* H, int pos) {
+	DListNode* p = H;
+
+	for (int i = 1; i <= pos; i++)
+		p = p->next;
+
+	element e = p->data;
+
+	p->prev->next = p->next;
+	p->next->prev = p->prev;
+	free(p);
+
+	return e;
+}
+
+void print(DListNode* H, DListNode* T) {
+	for (DListNode* p = H->next; p != T; p = p->next)
+		printf("[%d] <=> ", p->data);
+	printf("\b\b\b\b   \n");
+}
+
+int main() {
+	DListNode* H = (DListNode*)malloc(sizeof(DListNode));
+	DListNode* T = (DListNode*)malloc(sizeof(DListNode));
+
+	init(H, T);
+
+	insert(H, 1, 10); print(H, T);
+	insert(H, 1, 30); print(H, T);
+	insert(H, 2, 20); print(H, T);
+	insert(H, 3, 40); print(H, T);
+	insert(H, 4, 50); print(H, T);
+	insert(H, 2, 90); print(H, T);
+
+	delete(H, 5); print(H, T);
+	delete(H, 1); print(H, T);
+	delete(H, 2); print(H, T);
+	delete(H, 2); print(H, T);
+
+
+	return 0;
+}
