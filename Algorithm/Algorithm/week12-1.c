b5d4342f89f363b87d7f17ff75dc053fdd576241
parkjh21c <jiho010920@gmail.com>
2024-11-22 22:09:17 +0900

    week12

--- Algorithm/Algorithm/week12-1.c
+++ Algorithm/Algorithm/week12-1.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 
 typedef struct GraphNode {
-	char data;
-	struct GraphNode* link;
+	char vertex;
+	struct GraphNode* outEdges;
+	struct GraphNode* inEdges;
+	int inDegree;
 }GraphNode;
 
 int n, m;
@@ -16,7 +18,14 @@ void initializeGraph() {
 		G[i] = (GraphNode*)malloc(m * sizeof(GraphNode));
 }
 
-GraphNode** buildGraph(GraphNode** G) {
+void insertVertex(char vertex, int i) {
+	G[i]->vertex = vertex;
+	G[i]->outEdges = NULL;
+	G[i]->inEdges = NULL;
+	G[i]->inDegree = 0;
+}
+
+void buildGraph() {
 	initializeGraph();
 	scanf("%d", &n);
 
@@ -25,12 +34,17 @@ GraphNode** buildGraph(GraphNode** G) {
 		scanf("%c", &vertex); getchar();
 		insertVertex(vertex, i);
 	}
-	return G;
+}
+
+void topologicalSort() {
+
 }
 
 int main() {
 	
 	buildGraph();
+
+	topologicalSort();
 
 	scanf("%d", &l);
 
